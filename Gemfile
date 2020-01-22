bc1f393343723214de129a85085dc22143fde24e
Henrique Almeida <almeida.cof@gmail.com>
2020-01-22 17:59:36 -0300

    alteração de gems

--- Gemfile
+++ Gemfile
@@ -71,3 +71,4 @@ gem 'carrierwave'
 gem 'mini_magick'
 
 gem 'rails-i18n'
+gem 'ffaker'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
-# frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'ffaker'
+
+10.times do 
+    Setor.create(
+        empresa: Faker::first_name,
+        unidade: Faker::last_name,
+         )
+end
