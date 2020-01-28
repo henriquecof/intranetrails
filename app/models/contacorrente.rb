5c2e95d7bb73029fbc477bd37f0a785335bbc1b3
Henrique Almeida <almeida.cof@gmail.com>
2020-01-28 17:16:22 -0300

    configuração banco

--- app/models/contacorrente.rb
+++ app/models/contacorrente.rb
@@ -1,2 +1,10 @@
 class Contacorrente < ApplicationRecord
+    enum tipo: { Bancária: 0 , Caixa: 1 }
+    enum banco: {
+        "Banco do Brasil"=>  001,
+        "Caixa Econômica" => 104,
+        "Banco Santander" => 033
+    
+    
+    }
 end
